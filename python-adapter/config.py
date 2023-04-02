8f7f4a703deb286daaa7baee4899e4a8dd3a3fde
DarkFeed <129678163+Dark-Feed@users.noreply.github.com>
2023-04-02 20:43:40 +0300

    Create config.py

---
+++ python-adapter/config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#DarkFeed.io Adapter Configuration File
+#Mauro Eldritch @ DarkFeed - 2023
+
+#DarkFeed.io's Cookie
+cookie = "wordpress_logged_in_6112aa0faebb81b4843c7=User2%7C1678817627%7CuHhsbEL5lEnPn3TCk52trj1mjSa8g11fqLe3LlBKQgv%7C8ada458db6fabf84f6ce32401e28"
+
+#Output format: json, table, csv, text, yaml, pdf
+output_format = "table"
+
+#Output channel: file, stdout
+output_channel = "stdout"
