7f704e1b146cfcffa4ff413581bae64dd7823258
DarkFeed <129678163+Dark-Feed@users.noreply.github.com>
2023-04-02 20:47:13 +0300

    Create config.rb

---
+++ ruby-adapter/config.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/ruby
+#DarkFeed.io Adapter Configuration File
+#Mauro Eldritch @ DarkFeed - 2023
+
+#DarkFeed.io's Cookie
+$cookie = "wordpress_logged_in_6112aa0fae18964843c7=User1%7C1678817627%7CuHhsbEL5lEnPn3TCk52trj1mjSa8g11fqLe3Lgv%7C8ada4abf84f6ce3240384efaebc480f1e28"
+
+#Output format: json, table, csv, text, yaml, pdf
+$output_format = "table"
+
+#Output channel: file, stdout
+$output_channel = "stdout"
